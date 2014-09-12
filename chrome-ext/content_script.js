1957a02cabf4ac9a63ff5d4a7a5bd713454bb316
fourq <r@cky.bz>
2014-09-12 10:53:09 -0500

    finished chrome ext, firefox ad-on needs work [help]

--- chrome-ext/content_script.js
+++ chrome-ext/content_script.js
@@ -1,13 +1,31 @@
-$("[href*='/resource']").attr('href', setNewValue);
-$("[src*='/resource']").attr('src', setNewValue);
-$("script[src*='/resource']").attr('src', displayValue);
-
-console.log($(location).attr('search'));
-
-function setNewValue(index, attrValue) {
-  return ('https://localhost:8000' + attrValue);
+var port = gup('port');
+if (!isNaN(port) && (port <= 65535) && (port > 0)) {
+  $("[href*='/resource']").attr('href', setNewValue);
+  $("[src*='/resource']").attr('src', setNewValue);
+  $("script[src*='/resource']").attr('src', replaceScript);
+} else {
+  console.log('invalid port');
 }
 
-function displayValue(index, attrValue) {
-  console.log(index + ' ' + attrValue);
+function setNewValue(index, attrValue) {
+  return ('https://localhost:' + port + attrValue);
+}
+
+function replaceScript(index, attrValue) {
+  if (attrValue.indexOf('localhost:') >= 0) {
+    $("script[src*='" + attrValue + "']").replaceWith('<script src="'
+      + attrValue + '" type="text/javascript">');
+    return;
+  }
+}
+
+function gup(name) {
+  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+  var regexS = "[\\?&]" + name + "=([^&#]*)";
+  var regex = new RegExp(regexS);
+  var results = regex.exec(window.location.href);
+  if(results == null)
+    return null;
+  else
+    return results[1];
 }
